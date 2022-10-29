15b207e90e9c699d89766e87502671f4c09118ed
Tomas Doran <tdoran@yelp.com>
2022-10-29 22:04:03 +0100

    Move stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, render::texture::ImageSettings, window::PresentMode, math::vec3};
+use bevy::{prelude::*, render::texture::ImageSettings, window::PresentMode};
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, fs::File};
 
@@ -53,53 +53,7 @@ impl Cursor {
     }
 }
 
-fn animate_cursor(
-    time: Res<Time>,
-    mut query: Query<(
-        &mut AnimationTimer,
-        &mut TextureAtlasSprite,
-        &mut Cursor,
-    )>,
-) {
-    for (mut timer, mut sprite, mut cursor) in &mut query {
-        timer.tick(time.delta());
-        if timer.just_finished() {
-            if cursor.flash || !cursor.visible {
-                cursor.flash = false;
-                sprite.color.set_a(0.0);
-            } else {
-                cursor.flash = true;
-                sprite.color.set_a(1.0);
-            }
-        }
-    }
-}
 
-fn keyboard_input(
-    keys: Res<Input<KeyCode>>,
-    mut query: Query<(
-        &mut Transform,
-        &mut Cursor,
-    )>,
-) {
-    for (mut transform, _cursor) in &mut query {
-        let mut new_cursor_position = transform.translation;
-        if keys.just_pressed(KeyCode::Left) {
-            new_cursor_position = transform.translation - vec3(SCALED_SPRITE_SIZE, 0.0, 0.0);
-        }
-        if keys.just_pressed(KeyCode::Right) {
-            new_cursor_position = transform.translation + vec3(SCALED_SPRITE_SIZE, 0.0, 0.0);
-        }
-        if keys.just_pressed(KeyCode::Up) {
-            new_cursor_position = transform.translation + vec3(0.0, SCALED_SPRITE_SIZE, 0.0);
-        }
-        if keys.just_pressed(KeyCode::Down) {
-            new_cursor_position = transform.translation - vec3(0.0, SCALED_SPRITE_SIZE, 0.0);
-        }
-        transform.translation.x = new_cursor_position.x.clamp(-SCREEN_WIDTH/2.0+HALF_SPRITE+SCALED_SPRITE_SIZE, SCREEN_WIDTH/2.0-HALF_SPRITE-SCALED_SPRITE_SIZE);
-        transform.translation.y = new_cursor_position.y.clamp(-SCREEN_HEIGHT/2.0+HALF_SPRITE+SCALED_SPRITE_SIZE, SCREEN_HEIGHT/2.0-HALF_SPRITE-SCALED_SPRITE_SIZE);
-    }
-}
 
 #[derive(Component)]
 struct Position { x: u8, y: u8 }
@@ -305,17 +259,22 @@ mod menu {
 }
 
 mod game {
-    use bevy::prelude::*;
+    use bevy::{prelude::*, math::vec3};
 
     pub struct GamePlugin;
 
-    use super::{GameState, despawn_screen, spawn_anim, Game, AnimationTimer, RepeatAnimation, Mortal};
+    use super::{GameState, despawn_screen, spawn_anim, Game, AnimationTimer, RepeatAnimation, Mortal, Cursor, SCALED_SPRITE_SIZE, SCREEN_HEIGHT, SCREEN_WIDTH, HALF_SPRITE};
 
     impl Plugin for GamePlugin {
         fn build(&self, app: &mut App) {
             app
                 .add_system_set(SystemSet::on_enter(GameState::Game).with_system(game_setup))
-                .add_system_set(SystemSet::on_update(GameState::Game).with_system(animate_sprite))
+                .add_system_set(
+                    SystemSet::on_update(GameState::Game)
+                        .with_system(animate_sprite)
+                        .with_system(animate_cursor)
+                        .with_system(keyboard_input)
+                    )
                 .add_system_set(
                     SystemSet::on_exit(GameState::Game)
                         .with_system(despawn_screen::<OnGameScreen>),
@@ -330,6 +289,7 @@ mod game {
     fn game_setup(mut commands: Commands, g: Res<Game>) {
         spawn_anim(&mut commands, g.tah.clone(), Vec2::splat(2.0), 120, 8);
         spawn_anim(&mut commands, g.tah.clone(), Vec2::splat(1.0), 180, 4);
+        Cursor::new(g.tah.clone(), &mut commands);
     }
 
     fn animate_sprite(
@@ -359,6 +319,54 @@ mod game {
                     sprite.index = repeater.max + 1;
                 }
             }
+        }
+    }
+
+    fn animate_cursor(
+        time: Res<Time>,
+        mut query: Query<(
+            &mut AnimationTimer,
+            &mut TextureAtlasSprite,
+            &mut Cursor,
+        )>,
+    ) {
+        for (mut timer, mut sprite, mut cursor) in &mut query {
+            timer.tick(time.delta());
+            if timer.just_finished() {
+                if cursor.flash || !cursor.visible {
+                    cursor.flash = false;
+                    sprite.color.set_a(0.0);
+                } else {
+                    cursor.flash = true;
+                    sprite.color.set_a(1.0);
+                }
+            }
+        }
+    }
+    
+    fn keyboard_input(
+        keys: Res<Input<KeyCode>>,
+        mut query: Query<(
+            &mut Transform,
+            &mut Cursor,
+        )>,
+    ) {
+        for (mut transform, _cursor) in &mut query {
+            let mut new_cursor_position = transform.translation;
+            if keys.just_pressed(KeyCode::Left) {
+                new_cursor_position = transform.translation - vec3(SCALED_SPRITE_SIZE, 0.0, 0.0);
+            }
+            if keys.just_pressed(KeyCode::Right) {
+                new_cursor_position = transform.translation + vec3(SCALED_SPRITE_SIZE, 0.0, 0.0);
+            }
+            if keys.just_pressed(KeyCode::Up) {
+                new_cursor_position = transform.translation + vec3(0.0, SCALED_SPRITE_SIZE, 0.0);
+            }
+            if keys.just_pressed(KeyCode::Down) {
+                new_cursor_position = transform.translation - vec3(0.0, SCALED_SPRITE_SIZE, 0.0);
+            }
+            transform.translation.x = new_cursor_position.x.clamp(-SCREEN_WIDTH/2.0+HALF_SPRITE+SCALED_SPRITE_SIZE, SCREEN_WIDTH/2.0-HALF_SPRITE-SCALED_SPRITE_SIZE);
+            transform.translation.y = new_cursor_position.y.clamp(-SCREEN_HEIGHT/2.0+HALF_SPRITE+SCALED_SPRITE_SIZE, SCREEN_HEIGHT/2.0-HALF_SPRITE-SCALED_SPRITE_SIZE);
         }
     }
 }
