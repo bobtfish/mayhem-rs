300dbe3f6e5a145a87434316a30ff3a19359574b
Tomas Doran <tdoran@yelp.com>
2022-10-31 22:35:04 +0000

    Drawing text

--- src/menu.rs
+++ src/menu.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use super::{GameState, despawn_screen, Game, get_border};
+use super::{GameState, despawn_screen, Game, get_border, get_sprite_sheet_bundle};
 
 pub struct MenuPlugin;
 
@@ -23,6 +23,9 @@ impl Plugin for MenuPlugin {
 
 fn menu_setup(mut commands: Commands, g: Res<Game>) {
     get_border(&mut commands, g.tah.clone());
+    print_text("Hello", commands, g.fah.clone(), Vec2::new(1.0, 10.0))
+    //commands.spawn_bundle(get_sprite_sheet_bundle(g.fah.clone(), Vec2::new(1.0, 10.0), 0));
+
 }
 
 fn keyboard_input_system(keyboard_input: Res<Input<KeyCode>>, mut state: ResMut<State<GameState>>) {
@@ -31,4 +34,25 @@ fn keyboard_input_system(keyboard_input: Res<Input<KeyCode>>, mut state: ResMut<
         info!("'A' just pressed");
         state.set(GameState::Game).unwrap();
     }
+}
+
+fn print_text(str: &str, mut commands: Commands, fah: Handle<TextureAtlas>, v: Vec2) {
+    for (i,ch) in str.chars().enumerate() {
+        println!("{} {}", i, ch);
+        let mut newV = v.clone();
+        newV.x = newV.x + (i as f32/2.0);
+        commands.spawn_bundle(get_sprite_sheet_bundle(fah.clone(), newV, char_to_pos(ch)));
+    }
+}
+
+fn char_to_pos(c: char) -> usize {
+    println!("C IS {}", c);
+    let d = c as u32;
+    if d >=33 && d <= 126{
+        return (d - 31) as usize;
+    }
+    if c == ' ' {
+        return 1;
+    }
+    return 0;
 }
